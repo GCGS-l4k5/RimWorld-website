73168ed02b107d661c5a06b2ce7ef62c5458db1e
Therealredl4k5 <84022277+Therealredl4k5@users.noreply.github.com>
2023-03-16 12:09:54 +1100

    Added a lot

--- script.js
+++ script.js
@@ -27,3 +27,25 @@ const observer = new IntersectionObserver(entries => {
 });
 
 observer.observe(document.querySelector('.bodyParagraphP'))
+
+const gameplayImage = document.getElementById("backgroundSlideShow")
+var slideIndex = 0;
+var slideImage = ["url(images/background1.jpg)", "url(images/gameplay.jpg)"]
+
+function changeGameplay(){
+    if(slideIndex == 0){
+        gameplayImage.style.backgroundImage=slideImage[0];
+        console.log("Switch images to", slideImage[0])
+        slideIndex = 1;
+    }
+    else if(slideIndex == 1){
+        gameplayImage.style.backgroundImage=slideImage[1];
+        console.log("Switch images to", slideImage[1])
+        slideIndex = 0;
+    }
+    setTimeout(changeGameplay, 5000);
+}
+
+changeGameplay();
+ 
+
